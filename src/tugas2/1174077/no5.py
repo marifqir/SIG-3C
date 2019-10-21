3b8451895e2b8f233fe19c663ee5e6aeff0db8ae
alvan <alvanzah99@gmail.com>
2019-10-21 20:26:38 +0700

    Alvan

---
+++ src/tugas2/1174077/no5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 15:08:02 2019
+
+@author: ASUS
+"""
+
+import shapefile # import library shapefile
+w = shapefile.Writer('no5', shapeType=3)  # menggunakan fungsi writer yang digunakan untuk menggambar dan akan dinamakan no5 dalam bentuk shapetype=3 yang merupakan sebuah polyline
+ 
+w.field("kolom1","C")  # Digunakan untuk membuat kolom pertama pada table
+w.field("kolom2","C")  # Digunakan untuk membuat kolom dua pada table
+ 
+w.record("ngek","satu") # mengisi kolum yang sudah dibuat dengan ngek dan satu
+ 
+w.line([[[1,5],[5,5],[5,1],[3,3],[1,1]]]) # menghubungkan garis dengan mendefinisikan setiap titik awal dan akhir dari garis tersebut
+
+
+w.close() # Menutup writer
